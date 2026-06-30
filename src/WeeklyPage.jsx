cb19350bfc955f8fab720df5a244339dbbf07570
kmmhtpl16-jpg <kmmh.tpl16@gmail.com>
2026-06-30 13:55:14 +0700

    slip: ลากิจคนทดลองงาน แสดง ยังไม่ได้สิทธิ์

--- src/WeeklyPage.jsx
+++ src/WeeklyPage.jsx
@@ -487,7 +487,9 @@ export default function WeeklyPage({ role }) {
           '<div class="row mini2"><span>\u0e1b\u0e23\u0e30\u0e01\u0e31\u0e19\u0e07\u0e32\u0e19\u0e2a\u0e30\u0e2a\u0e21</span><span>' + money(insB) + ' \u0e1a\u0e32\u0e17</span></div>' +
           '<div class="leave-h">\u0e2a\u0e34\u0e17\u0e18\u0e34\u0e4c\u0e01\u0e32\u0e23\u0e25\u0e32</div>' +
           leaveLine('\u0e25\u0e32\u0e1b\u0e48\u0e27\u0e22', lm.sick.days, lm.sick.dates, lsm.sick_remaining) +
-          leaveLine('\u0e25\u0e32\u0e01\u0e34\u0e08', lm.personal.days, lm.personal.dates, lsm.personal_remaining);
+          ((lsm.personal_quota == null || Number(lsm.personal_quota) === 0)
+            ? '<div class="row mini2"><span>\u0e25\u0e32\u0e01\u0e34\u0e08 \u2014 \u0e22\u0e31\u0e07\u0e44\u0e21\u0e48\u0e44\u0e14\u0e49\u0e2a\u0e34\u0e17\u0e18\u0e34\u0e4c (\u0e17\u0e14\u0e25\u0e2d\u0e07\u0e07\u0e32\u0e19)</span><span></span></div>'
+            : leaveLine('\u0e25\u0e32\u0e01\u0e34\u0e08', lm.personal.days, lm.personal.dates, lsm.personal_remaining));
         return `
           ${baseInfo}
           <div class="cols">
